ff97309dffaefbc90e35e04e9b593090615e870a
Marcel-MD <marcelvlasenco@gmail.com>
2022-09-13 17:45:39 +0300

    Switch to gorilla mux

--- main.go
+++ main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"encoding/json"
-	"io/ioutil"
+	"io"
+	"net/http"
 	"os"
 
 	"github.com/Marcel-MD/kitchen/domain"
-	"github.com/gin-gonic/gin"
+	"github.com/gorilla/mux"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 )
@@ -20,21 +21,21 @@ func main() {
 	orderList := domain.NewOrderList(orderChan, menu)
 	orderList.Run()
 
-	r := gin.Default()
-	r.POST("/order", func(c *gin.Context) {
+	r := mux.NewRouter()
+	r.HandleFunc("/order", func(w http.ResponseWriter, r *http.Request) {
 		var order domain.Order
-
-		if err := c.ShouldBindJSON(&order); err != nil {
-			log.Err(err).Msg("Error binding JSON")
-			c.JSON(400, gin.H{"error": err.Error()})
+		err := json.NewDecoder(r.Body).Decode(&order)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
 
 		orderChan <- order
 
-		c.JSON(200, gin.H{"message": "Order received"})
-	})
-	r.Run(":8081")
+		w.WriteHeader(http.StatusOK)
+	}).Methods("POST")
+
+	http.ListenAndServe(":8081", r)
 }
 
 func config() domain.Config {
@@ -47,7 +48,7 @@ func config() domain.Config {
 	}
 	defer file.Close()
 
-	byteValue, _ := ioutil.ReadAll(file)
+	byteValue, _ := io.ReadAll(file)
 	var cfg domain.Config
 	json.Unmarshal(byteValue, &cfg)
 
